2018184d8c5eb3a103df519d6cf3afa527e3bed9
angelkwatra <143274756+angelkwatra@users.noreply.github.com>
2025-01-30 22:59:57 +0530

    L3q2.java

--- LAB3/L3q2.java
+++ LAB3/L3q2.java
@@ -1,16 +1,24 @@
+import java.util.*;
 
 class L3q2 {
     public static int majorityElement(int[] nums) {
         int count = 0, candidate = 0;
-        for (int num : nums) {
-            if (count == 0) candidate = num;
-            count += (num == candidate) ? 1 : -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+            }
+            if (nums[i] == candidate) {
+                count = count + 1;
+            } else {
+                count = count - 1;
+            }
         }
         return candidate;
     }
 
     public static void main(String[] args) {
         int[] nums = {2, 2, 1, 1, 1, 2, 2};
-        System.out.println(majorityElement(nums)); 
+        System.out.println(Arrays.toString(new int[]{majorityElement(nums)}));
     }
 }
