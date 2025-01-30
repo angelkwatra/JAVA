bae97187909ba63fe22de0a5e53987290c09d0c1
angelkwatra <143274756+angelkwatra@users.noreply.github.com>
2025-01-30 23:00:58 +0530

    L3q4.java

--- LAB3/L3q4.java
+++ LAB3/L3q4.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class L3q4 {
     public static String reverseWords(String s) {
-        String[] words = s.trim().split("\\s+");
+        String[] words = s.split("\\s+");
         Collections.reverse(Arrays.asList(words));
         return String.join(" ", words);
     }
